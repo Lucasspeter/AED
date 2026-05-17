f0c0fb53b92f677046255c7598374c64738dff5d
Lucasspeter <luquinhas.s.peter@gmail.com>
2026-05-17 19:48:45 -0300

    trabalho avaliativo 2

---
+++ AEDavaliativo2/trabalhoaula2.c
@@ -0,0 +1,18 @@
+int* nextGreaterElements( int* nums, int numsSize, int* returnSize ) {
+    int* ans = ( int* ) malloc ( sizeof ( int ) * numsSize );
+    int pilha[ numsSize ];
+    int top = -1;
+    
+    
+
+    for ( int i = 2 * numsSize - 1; i >= 0; i-- ) {
+        while ( top != -1 && nums[pilha[ top ] ] <= nums[ i % numsSize ] ){
+            top--;
+        }
+        ans[ i % numsSize ] = top == -1 ? -1 : nums[pilha[ top ]];
+        pilha[ ++top ] = i % numsSize;
+    }
+
+    *returnSize = numsSize;
+    return ans;
+}
