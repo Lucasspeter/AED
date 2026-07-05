49ed359f0a44def6b2d67e95e0457fb109eef609
Lucasspeter <luquinhas.s.peter@gmail.com>
2026-07-04 23:23:20 -0300

    trabalhoaed3

--- Livre/1.c
+++ Livre/1.c
@@ -14,13 +14,14 @@ char n[30];
 char variavel;
 int cont = 0;
 
-pessoa *p1;
-p1 = (pessoa*)malloc(sizeof(pessoa));
+pessoa *p1 = NULL;
 
 printf("voce deseja adicionar um funcionario?\n(resposta s/n): ");
-scanf(" %c", &variavel);
+if (scanf(" %c", &variavel) != 1) {
+    return 1;
+}
 
-if(variavel == 'n'){
+if (variavel == 'n'){
     printf("#------------------------------#\n");
     printf("Nenhum funcionario adicionado.\n");
     printf("Programa encerrado.\n");
@@ -29,34 +30,39 @@ if(variavel == 'n'){
 }
 
 while(variavel != 'n'){
+    pessoa *temp = realloc(p1, (cont + 1) * sizeof *temp);
+    if (temp == NULL) {
+        free(p1);
+        printf("Erro de alocacao de memoria.\n");
+        return 1;
+    }
+    p1 = temp;
 
-p1 = realloc(p1, (cont + 1) * sizeof(pessoa));
+    printf("digite a idade do funcionario: ");
+    scanf("%d", &p1[cont].idade);
+    printf("digite o salario do funcionario: ");
+    scanf("%f", &p1[cont].salario);
 
-printf("digite a idade do funcionario: ");
-scanf("%d", &p1[cont].idade);
-printf("digite o salario do funcionario: ");
-scanf("%f", &p1[cont].salario);
+    getchar();
 
-getchar();
+    printf("Digite o nome do funcionario: ");
+    fgets(n, sizeof n, stdin);
 
-printf("Digite o nome do funcionario: ");
-fgets(n, 29, stdin);
+    n[strcspn(n, "\n")] = '\0';
+    strcpy(p1[cont].nome, n);
 
-n[strcspn(n, "\n")] = '\0';
-strcpy(p1[cont].nome, n);
+    cont++;
 
-cont++;
-
-printf("voce deseja adicionar outro funcionario?\n(resposta s/n): ");
-scanf(" %c", &variavel);
-
-
+    printf("voce deseja adicionar outro funcionario?\n(resposta s/n): ");
+    if (scanf(" %c", &variavel) != 1) {
+        break;
+    }
 };
 
 
 for(int i = 0; i < cont; i++){
     printf("#------------------------------#\n");
-    printf(("Funcionario %d\n"), i + 1);
+    printf("Funcionario %d\n", i + 1);
     printf("nome: %s\nidade: %d\nsalario: %f\n",p1[i].nome,p1[i].idade, p1[i].salario);
     printf("#------------------------------#\n");
 }
